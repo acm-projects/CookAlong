b071c9c931d337dcb37b51d0c2045763dc60a9fa
nam-t24 <namtruong831@gmail.com>
2020-10-07 18:25:31 -0500

    Added voice accents and gender

--- T2S.rb
+++ T2S.rb
@@ -1,12 +1,17 @@
-
+#use gem install google-cloud-text_to_speech
 require "google/cloud/text_to_speech"
 
 client = Google::Cloud::TextToSpeech.text_to_speech
 
 
-synthesis_input = { text: "You wanna see some real speed" }
+synthesis_input = { text: "Ingredients: 10 pounds of water, 2 apples" }
 
+#Accent language codes: 
+#US:          "en-US"
+#Australian:  "en-AU"
+#UK:          "en-GB"
 
+#genders: MALE, FEMALE
 voice = {
   language_code: "en-US",
   ssml_gender:   "FEMALE"
