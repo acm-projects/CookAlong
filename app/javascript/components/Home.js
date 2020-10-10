ddae7c096c7716493f4c21d16c91856ff8a87389
ryannd <ryandimaranan23@gmail.com>
2020-10-10 16:29:14 -0500

    new header

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -6,13 +6,13 @@ export default function Home() {
     return (
       <section className="hero is-dark is-fullheight hero-parent">
         <div className="bg"></div>
-        <div className="hero-body mx-0 my-0 px-0 py-0">
+        <div className="hero-body mx-0 my-0 px-0 py-0" >
             <div className="container columns is-fluid is-centered is-multiline mx-0 my-0 px-0 py-0 ">
                 <div className="column is-full has-text-justified mx-0 my-0 px-0 py-0">
                     <p className="title-home">CookAlong</p>
                 </div>
                 <div className="column is-full field has-addons has-addons-centered">
-                <input class="input is-rounded" type="text" placeholder="Search for a recipe"></input>
+                <input className="input is-rounded" type="text" placeholder="Search for a recipe" style={{width: "70vw"}}></input>
                 </div>
                 <div className="column is-full field has-addons has-addons-centered">
                     <Link to="/search"><Button className="home-button" color="light">Search</Button></Link>
